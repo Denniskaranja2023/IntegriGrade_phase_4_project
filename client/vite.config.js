69db573cf5e17a370a34f95dbbe8411c423b5732
Denniskaranja2023! <denniswanyeki2021@gmail.com>
2025-09-22 14:12:48 +0300

    initiate a react frontend(client repo) using vite and add a proxy, create backend setup(server) and a virtual pipenv environment

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+// vite.config.js
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5555',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
