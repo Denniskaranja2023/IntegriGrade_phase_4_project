95e0f11e7f4bf1ac49c175c349182c247648770b
Denniskaranja2023! <denniswanyeki2021@gmail.com>
2025-09-23 13:07:19 +0300

    Initial migrations to create database tables

---
+++ server/app.py
@@ -0,0 +1,10 @@
+from config import app
+from models import *
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- server/config.py
+++ server/config.py
@@ -3,7 +3,6 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Metadata
 from flask_cors import CORS
 from sqlalchemy import MetaData
 
